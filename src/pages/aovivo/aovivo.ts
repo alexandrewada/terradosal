a6c8495f65e1c2d44c56e2835048ef51e0b98359
Ingles200h <alexandre.rwada@gmail.com>
2018-08-24 11:47:14 -0300

    Ult atualizao

--- src/pages/aovivo/aovivo.ts
+++ src/pages/aovivo/aovivo.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media';
 
 /**
  * Generated class for the AovivoPage page.
@@ -15,11 +16,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AovivoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(  private streamingMedia: StreamingMedia,public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  startVideo() {
+    let options: StreamingVideoOptions = {
+      successCallback: () => { console.log('Video played') },
+      errorCallback: (e) => { console.log('Error streaming') },
+      orientation: 'landscape',
+      shouldAutoClose: true,
+      controls: false
+    };
+
+    this.streamingMedia.playVideo('http://srv9.zoeweb.tv:1935/zw9009/stream/playlist.m3u8', options);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AovivoPage');
+    this.startVideo();
   }
 
 }
